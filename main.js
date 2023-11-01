4b75d7e6e27b7095df6a1d00f21f0bb8466627ab
zackaids <zalkar.ai77@gmail.com>
2023-11-01 19:25:35 -0400

    tried to do the ripple button effect

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // main.js
+tslint:disabled
 
 const buttons = document.querySelector('.buttons');
 console.log(buttons.children);
@@ -7,4 +8,38 @@ for (const child of buttons.children) {
 }
 function handleClick(e) {
     window.location.pathname = `${e.target.className}.html`;
+}
+
+const goToMain = document.querySelector('.nav-logo');
+
+goToMain.addEventListener("click", clickImg);
+function clickImg() {
+    document.location.pathname = '/index.html'
+}
+
+function createRipple(event) {
+
+}
+
+const rippleButton = event.currentTarget;
+const circle = document.createElement("span");
+const diameter = Math.max(button.clientWidth, button.clientHeight);
+const radius = diameter / 2;
+
+circle.style.width = circle.style.height = `${diameter}px`;
+circle.style.left = `${event.clientX - (button.offsetLeft + radius)}px`;
+circle.style.top = `${event.clientY - (button.offsetTop + radius)}px`;
+circle.classList.add("ripple");
+
+const ripple = button.getElementByClassName("ripple")[0];
+
+if (ripple) {
+    ripple.remove();
+}
+
+button.appendChild(circle);
+
+const rippleButtons = document.getElementsByClassName("buttons");
+for (const rippbleButton of rippleButtons) {
+    document.addEventListener("click", createRipple);
 }
